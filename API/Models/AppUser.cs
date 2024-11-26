d071c912567d89f68aeb60cd7b226cb2229d0812
jan.pierzchala <jan.pierzchala@idealogic.pl>
2024-11-26 16:57:23 +0100

    First Commit

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
